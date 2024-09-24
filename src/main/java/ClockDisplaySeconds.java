31847d6148418ecfaa3be85459bfe6cb39d29866
farahtameemi <irenicgazexx@gmail.com>
2024-09-23 22:17:41 -0700

    Update ClockDisplaySeconds.java

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,38 +1,38 @@
 
 public class ClockDisplaySeconds extends ClockDisplay {
-    private NumberDisplay seconds;
+    private NumberDisplay seconds; // Secs display
 
     public ClockDisplaySeconds() {
-        super();
-        seconds = new NumberDisplay(60);
+        super(); // Init parent
+        seconds = new NumberDisplay(60); // 60 secs
     }
 
     public ClockDisplaySeconds(int hour, int minute, int second) {
-        super(hour, minute);
-        seconds = new NumberDisplay(60);
-        setTime(hour, minute, second);
+        super(hour, minute); // Set hr/min
+        seconds = new NumberDisplay(60); // 60 secs
+        setTime(hour, minute, second); // Set all
     }
 
     public void timeTick() {
-        seconds.increment();
+        seconds.increment(); // Tick secs
         if (seconds.getValue() == 0) {
-            super.timeTick();
+            super.timeTick(); // Tick hr/min if sec wraps
         }
-        updateDisplay();
+        updateDisplay(); // Refresh display
     }
 
     public void setTime(int hour, int minute, int second) {
-        super.setTime(hour, minute);
-        seconds.setValue(second);
-        updateDisplay();
+        super.setTime(hour, minute); // Set hr/min
+        seconds.setValue(second); // Set secs
+        updateDisplay(); // Refresh display
     }
 
     public String getTime() {
-        return super.getTime() + ":" + seconds.getDisplayValue();
+        return super.getTime() + ":" + seconds.getDisplayValue(); // Add secs to time
     }
 
     private void updateDisplay() {
-        // Override to include seconds
-        super.updateDisplay();
+        // Override to include secs
+        super.updateDisplay(); // Keep parent display logic
     }
 }
